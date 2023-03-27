6012611fd669b4b1e8326d513180d54c5eca16f5
ACB <ACBBible@gmail.com>
2023-03-27 18:27:10 -0400

    fix dropdown boxes

--- JS/main.js
+++ JS/main.js
@@ -1,31 +1,61 @@
+function acbVersion() {
+    alert('test');
+    acbClose();
+}
+
+function acbSetOldTestament() {
+
+    document.getElementById("id-acbTestament").textContent = "Old Testament"
+};
+
+function acbSetNewTestament() {
+
+    document.getElementById("id-acbTestament").textContent = "New Testament"
+};
 
 function acbOpenBook() {
-    let element = this.event.target;
 
     this.event.stopImmediatePropagation();
+    this.event.preventDefault();
     if (bookOpen === 0) {
         document.getElementById("id-acbBook").style.display = "block";
+        document.getElementById("id-acbFixedPanel").style.display = "block";
         bookOpen = 1;
     } else {
         document.getElementById("id-acbBook").style.display = "none";
+        document.getElementById("id-acbFixedPanel").style.display = "none";
         bookOpen = 0;
     };
+};
 
-}
-function acbSortBook() {
-
-    let element = this.event.target;
+function acbOpenVersion() {
 
     this.event.stopImmediatePropagation();
-    if (upDown === 0) {
-        element.classList.remove("down");
-        element.classList.add("up");
-        element.setAttribute("title","Sort Books Biblically");
-        upDown = 1;
+    this.event.preventDefault();
+    if (versionOpen === 0) {
+        document.getElementById("id-acbVersion").style.display = "block";
+        document.getElementById("id-acbFixedPanel").style.display = "block";
+        versionOpen = 1;
     } else {
-        element.classList.remove("up");
-        element.classList.add("down");
-        element.setAttribute("title","Sort Books Alphabetically");
-        upDown = 0;
+        document.getElementById("id-acbVersion").style.display = "none";
+        document.getElementById("id-acbFixedPanel").style.display = "none";
+        versionOpen = 0;
     };
-}
+};
+
+function acbClose() {
+
+    if (bookOpen === 1) {
+        document.getElementById("id-acbBook").style.display = "none";
+        document.getElementById("id-acbFixedPanel").style.display = "none";
+        bookOpen = 0;
+        return;
+    }
+
+    if (versionOpen === 1) {
+        document.getElementById("id-acbVersion").style.display = "none";
+        document.getElementById("id-acbFixedPanel").style.display = "none";
+        versionOpen = 0;
+        return;
+    };
+};
--- JS/variables.js
+++ JS/variables.js
@@ -1,3 +1,8 @@
 var mainDataPath = 'https://github.com/ACB-Bible/AmericanComputerizedBible/';
 var upDown = 0;
+//var open key nothing = 0, book = 1, chapter = 2, verse = 3, version = 4
+var open = 0;
 var bookOpen = 0;
+var chapterOpen = 0;
+var vreseOpen = 0;
+var versionOpen = 0;
