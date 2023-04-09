55cf93457d7d2bfecd404bc08e22cd9e49d81a4b
ACB <ACBBible@gmail.com>
2023-04-09 12:54:57 -0400

    SEO Optimize

--- JS/variables.js
+++ JS/variables.js
@@ -1,11 +1,18 @@
 var mainPath = "https://acb-bible.github.io/AmericanComputerizedBible/";
 
+//testment old testament = 0 new testament = 1
+var testament = 0;
 var versionOpen = 0;
 var versionActive = 'TWF';
 var versionClicked = 'id-acbVrsn1';
 
 var bookOpen = 0;
 var bookClicked = 'id-acbBk1';
+var bookSorted = false;
+var oldBooks = [];
+var oldAlph;
+var newBooks = [];
+var newAlph;
 
 var chapterOpen = 0;
 var chapterClicked = 'id-acbChp1';
@@ -13,11 +20,6 @@ var chapterCount = 50;
 
 var verseOpen = 0;
 var verseClicked = 'id-acbVrs1';
-
-//testment old testament = 0 new testament = 1
-var testament = 0;
-var oldBooks = [];
-var newBooks = [];
 var allVerses = [];
 var verses;
 
