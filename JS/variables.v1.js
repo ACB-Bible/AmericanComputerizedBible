9b91f27697d6453e870331a20a01d350f8ac18aa
ACB <ACBBible@gmail.com>
2023-05-05 19:06:56 -0400

    Update variables.v1.js

--- JS/variables.v1.js
+++ JS/variables.v1.js
@@ -1,4 +1,4 @@
-var mainPath = "https://www.acbible.com/";
+var mainPath = "https://acbible.com/";
 //var mainPath = "https://acbible.github.io/AmericanComputerizedBible/"
 //var mainPath = "./";
 //testment old testament = 0 new testament = 1
